84aa57546a1c3889ae86e5c5b6885552f66048eb
n <n@pop-os.localdomain>
2023-06-16 12:43:51 -0600

    cli create and load commands

--- main.go
+++ main.go
@@ -1,56 +1,56 @@
 package main
 
 import (
-	"os"
-	"log"
-	"fmt"
 	"github.com/nikalmus/go-serialize-deserialize/point"
 	"github.com/nikalmus/go-serialize-deserialize/space"
+	"flag"
+	"fmt"
+	"log"
+	"os"
 )
 
 func main() {
 	dbPath := "./db/pointstore" 
 
-	// Create the directory if it doesn't exist
-	err := os.MkdirAll(dbPath, 0755)
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	s, err := space.InitSpace(dbPath)
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer s.Close()
 
-	p1 := &point.Point{X: 11, Y: 21}
-	p2 := &point.Point{X: 35, Y: 45}
+	createCmd := flag.NewFlagSet("create", flag.ExitOnError)
+	loadCmd := flag.NewFlagSet("load", flag.ExitOnError)
 
-	// Call AddPoint to save the points
-	err = s.AddPoint(p1)
-	if err != nil {
-		log.Fatal(err)
+	x := createCmd.Int("x", 0, "X coordinate")
+	y := createCmd.Int("y", 0, "Y coordinate")
+
+	if len(os.Args) < 2 {
+		fmt.Println("create or load command is required")
+		os.Exit(1)
 	}
 
-	err = s.AddPoint(p2)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Call LoadPoints to retrieve the saved points
-	points, err := s.LoadPoints()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Points in the database:")
-
-	for _, p := range points {
-		fmt.Printf("X: %d, Y: %d\n", p.X, p.Y)
-	}
-
-	err = s.Close()
-	if err != nil {
-		log.Fatal(err)
+	switch os.Args[1] {
+	case "create":
+		createCmd.Parse(os.Args[2:])
+		p := point.CreatePoint(int32(*x), int32(*y))
+		err := s.AddPoint(p)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Point created successfully!")
+	case "load":
+		loadCmd.Parse(os.Args[1:])
+		points, err := s.LoadPoints()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Points in the database:")
+		for _, p := range points {
+			fmt.Printf("X: %d, Y: %d\n", p.X, p.Y)
+		}
+	default:
+		fmt.Println("Unknown command:", os.Args[1])
+		os.Exit(1)
 	}
 }
 
